27fffdba52931e1c488ff7eb11a0ee2b653ed45d
kushuu <sonikushu007@gmail.com>
2021-03-18 01:55:14 +0530

    update

--- main_cogs.py
+++ main_cogs.py
@@ -7,7 +7,6 @@ TOKEN = os.getenv('TOKEN')
 
 bot = commands.Bot(command_prefix="--")
 
-# bot.load_extension(f'cogs.nerdy')
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py') and filename != "__init__.py":
             bot.load_extension(f'cogs.{filename[:-3]}')
