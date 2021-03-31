4dff8b03faf0c212b724272e8630955f3914cf44
kushuu <sonikushu007@gmail.com>
2021-04-01 00:38:53 +0530

    lat-long for US-states (definition done,call rem)

--- cogs/helpers/temp.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open('US_caps.json') as json_file:
-        data = json.load(json_file)
-        for i in data:
-            print('"' + i['abbr'].lower() + '"' + " : " + '"' + i['capital'] + '",')
-            print('"' + i['name'].lower() + '"' + " : " + '"' + i['capital'] + '",')
-
